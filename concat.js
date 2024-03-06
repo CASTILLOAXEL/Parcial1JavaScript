f19c0aa03ff6f0a79cd63772f85c19ff6a922bcf
CASTILLOAXEL <140232308+CASTILLOAXEL@users.noreply.github.com>
2024-03-06 10:26:01 -0600

    Delete concat.js

--- concat.js
+++
@@ -1,5 +0,0 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const array2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-
-const resultArray = array.concat(array2);
-console.log(resultArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
